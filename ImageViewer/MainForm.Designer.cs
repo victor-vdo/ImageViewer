7e1db82d4905b4f7764993e45c63a11606be5045
Victor Oliveira <victor.vdo@outlook.com>
2021-07-01 01:39:24 -0300

    Add project files.

---
+++ ImageViewer/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace ImageViewer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcbImage = new System.Windows.Forms.PictureBox();
+            this.lblSelect = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.openDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbImage
+            // 
+            this.pcbImage.BackColor = System.Drawing.Color.Silver;
+            this.pcbImage.Location = new System.Drawing.Point(41, 52);
+            this.pcbImage.Name = "pcbImage";
+            this.pcbImage.Size = new System.Drawing.Size(400, 300);
+            this.pcbImage.TabIndex = 0;
+            this.pcbImage.TabStop = false;
+            // 
+            // lblSelect
+            // 
+            this.lblSelect.AutoSize = true;
+            this.lblSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSelect.Location = new System.Drawing.Point(38, 24);
+            this.lblSelect.Name = "lblSelect";
+            this.lblSelect.Size = new System.Drawing.Size(127, 13);
+            this.lblSelect.TabIndex = 1;
+            this.lblSelect.Text = "Select the image file:";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(171, 19);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(31, 22);
+            this.btnSelect.TabIndex = 2;
+            this.btnSelect.Text = "...";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // openDialog
+            // 
+            this.openDialog.FileName = "openFileDialog1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(505, 378);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lblSelect);
+            this.Controls.Add(this.pcbImage);
+            this.Name = "MainForm";
+            this.Text = "Image Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbImage;
+        private System.Windows.Forms.Label lblSelect;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.OpenFileDialog openDialog;
+    }
+}
+
---
+++ ImageViewer/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Security;
+using System.Windows.Forms;
+
+namespace ImageViewer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            
+            this.openDialog.Multiselect = false;
+            openDialog.Title = "Select the Image File";
+            openDialog.InitialDirectory = @"C:\Users\";
+            openDialog.Filter = "All files (*.*)|*.*|"+"Images (*.BMP;*.JPG;*.GIF,*.PNG,*.TIFF)|*.BMP;*.JPG;*.GIF;*.PNG;*.TIFF";
+            openDialog.CheckFileExists = true;
+            openDialog.CheckPathExists = true;
+            openDialog.FilterIndex = 2;
+            openDialog.RestoreDirectory = true;
+            openDialog.ReadOnlyChecked = true;
+            openDialog.ShowReadOnly = true;
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in openDialog.FileNames)
+                {
+                    try
+                    {
+                        Image image = Image.FromFile(file);
+                        this.pcbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                        this.pcbImage.Image = image;  
+                    }
+                    catch (SecurityException ex)
+                    {
+                        MessageBox.Show("A security error was detected. Please, contact the software support.\n\n" +
+                                                    "Error mensage : " + ex.Message + "\n\n" +
+                                                    "Details (send to support):\n\n" + ex.StackTrace);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("The image could not be showed : " + file.Substring(file.LastIndexOf('\\'))
+                                                   + ". You don't have permission to read the file or " +
+                                                   "the file is corrupted.\n\nError message : " + ex.Message);
+                    }
+                }
+                
+            }
+        }
+    }
+}
